5a8d9b1a3964796a0013bb5bfffca354060ad99f
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-03 16:17:27 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260103/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void print1(){
+    cout << "12345^&*()_" << endl;
+}
+
+int main() {
+    cin >> N;
+    for(int i=0; i<N; i++){
+        print1();
+    }
+    // Please write your code here.
+
+    return 0;
+}

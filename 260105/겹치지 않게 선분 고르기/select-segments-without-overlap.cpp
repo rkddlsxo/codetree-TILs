352c5692d886af9aca6c90de0294a511b7d57d41
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-05 16:31:54 +0900

    Title: 겹치지 않게 선분 고르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260105/겹치지 않게 선분 고르기/select-segments-without-overlap.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int x1[15], x2[15];
+int count_max = 0;
+
+vector<int> answer; 
+
+void select(int num){
+    if(num == n){
+        int narray[1001] = {};
+        
+        for(int i=0; i<n; i++){
+            if(answer[i] == 1){
+                for(int j = x1[i]; j<x2[i]; j++){
+                narray[j]++;
+                if(narray[j] >= 2){
+                    return; 
+                }
+            }
+            }
+        }
+        int cnt = 0; 
+        for(int i=0; i<answer.size(); i++){
+            if(answer[i] == 1){
+                cnt++;
+            }
+        }
+        if(count_max < cnt){
+            count_max = cnt;
+        }
+        return;
+    }
+
+    for(int i=0; i<=1; i++){
+        answer.push_back(i);
+        select(num+1);
+        answer.pop_back();
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x1[i] >> x2[i];
+    }
+    select(0);
+    cout << count_max;
+    // Please write your code here.
+
+    return 0;
+}

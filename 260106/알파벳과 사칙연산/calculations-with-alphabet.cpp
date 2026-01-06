305232d749610f6940b60f7fc848f5439a211ad0
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-06 16:04:47 +0900

    Title: 알파벳과 사칙연산, Time: 11ms, Memory: 0MB, Status: Passed - Codetree

--- 260106/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 260106/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int expression_length;
 string expression;
-int number_list[6] = {};
+vector<int> number_list;
 vector<char> oper_list;
+int sum_max = -999999;
 
 int dustks(int a, int b, char c){
     if(c == '+'){
@@ -20,18 +21,54 @@ int dustks(int a, int b, char c){
     }
 }
 
-void calculate(int list[], char oper[], int expression_length){
-    int current_val = list[0];
+int calculate(){
+    vector<int> aa;
     for(int i=0; i<expression_length; i++){
         if(expression[i] == 'a'){
-
+            aa.push_back(number_list[0]);
         }
-        if(expression[i] == 'a'){
-            
+        if(expression[i] == 'b'){
+            aa.push_back(number_list[1]);
+        }
+        if(expression[i] == 'c'){
+            aa.push_back(number_list[2]);
+        }
+        if(expression[i] == 'd'){
+            aa.push_back(number_list[3]);
+        }
+        if(expression[i] == 'e'){
+            aa.push_back(number_list[4]);
+        }
+        if(expression[i] == 'f'){
+            aa.push_back(number_list[5]);
         }
     }
+
+    int temp_num = aa[0];
+    for(int i=0; i<expression_length/2; i++){
+        temp_num = dustks(temp_num, aa[i+1],oper_list[i]);
+    }
+
+    return temp_num;
+
 }
 
+
+void select(int num){
+    if(num == 6){
+        int temp = calculate();
+        if(temp > sum_max){
+            sum_max = temp;
+        }
+        return;
+    }
+
+    for(int i=1; i<=4; i++){
+        number_list.push_back(i);
+        select(num+1);
+        number_list.pop_back();
+    }
+}
 
 
 int main() {
@@ -42,6 +79,8 @@ int main() {
         oper_list.push_back(expression[i]);
     }
     // Please write your code here.
+    select(0);
+    cout << sum_max;
 
     return 0;
 }

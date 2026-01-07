8cf0c309feaf56944207aaa2f0860867236bce77
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-07 20:34:48 +0900

    Title: N개 중에 M개 뽑기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 260107/N개 중에 M개 뽑기/n-choose-m.cpp
+++ 260107/N개 중에 M개 뽑기/n-choose-m.cpp
@@ -11,16 +11,24 @@ void select(int num, int cnt){
     if(num == M){
         if(cnt == M){
             for(int i=0; i<M; i++){
-                cout << answer[M-i-1] << " ";
+                cout << answer[i] << " ";
             }
             cout << endl;
         }
 
         return;
     }
-    int p = 1;
+    
     for(int i=1; i<=N; i++){
-        answer.push_back(i);
+        int p = 1;
+
+        if(num == 0 || answer[num-1] < i){
+            answer.push_back(i);
+        }
+        else{
+            continue;
+        }
+        
         for(int j=0; j<num; j++){
             if(p == 0){
                 break;

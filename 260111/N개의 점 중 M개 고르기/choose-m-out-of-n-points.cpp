62113d958afa1ccb0e28aaf0862cb3bfeab2473b
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-11 20:13:50 +0900

    Title: N개의 점 중 M개 고르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260111/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
+++ 260111/N개의 점 중 M개 고르기/choose-m-out-of-n-points.cpp
@@ -1,10 +1,43 @@
 #include <iostream>
 #include <cmath>
+#include <vector> 
 
 using namespace std;
 
+int min_distance = 9999999;
 int n, m;
 int x[20], y[20];
+vector <int> point;
+
+void select(int num, int cnt){
+    if(num == n){
+        if(cnt == m){
+            int local_min = 999999;
+            for(int i=0; i<m; i++){
+                for(int j=i; j<m; j++){
+                    if(i == j){
+                        continue;
+                    }
+                    int distance = pow(x[point[i]] - x[point[j]],2)+pow(y[point[i]] - y[point[j]],2);
+                    if(distance < local_min){
+                        local_min = distance;
+                    }
+                }
+            }
+
+            if(local_min < min_distance){
+                min_distance = local_min;
+            }
+        }
+        return; 
+    }
+
+    point.push_back(num);
+    select(num+1, cnt+1);
+    point.pop_back();
+    select(num+1, cnt);
+}
+
 
 int main() {
     cin >> n >> m;
@@ -12,21 +45,8 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> x[i] >> y[i];
     }
-
-    int min_distance = 9999999;
-
-    for(int i=0; i<n; i++){
-        for(int j=i; j<n; j++){
-            if(i== j){
-                continue;
-            }
-            int distance = pow((x[i] - x[j]),2) + pow((y[i] - y[j]),2);
-            if(distance < min_distance){
-                min_distance = distance;
-            }
-        }
-    }
-
+   
+    select(0,0);
     cout << min_distance;
 
     // Please write your code here.

f993d43272cf5f0a950ece4f78f136daa9685a3c
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-12 01:38:38 +0900

    Title: 외판원 순회, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260112/외판원 순회/traveling-salesman-problem.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int n;
+int A[10][10];
+bool* visited = new bool[n];
+vector<int> answer;
+int min_cost = 99999999;
+
+void select(int num){
+    if(num == n){
+        int cost = 0;
+        cost += A[0][answer[0]];
+
+        for(int i=0; i<n-2; i++){
+            cost += A[answer[i]][answer[i+1]];
+ 
+        }        
+
+        cost += A[answer[n-2]][0];
+
+        if(min_cost > cost){
+            min_cost = cost;
+        }
+        return;
+    }
+
+    for(int i=1; i<n; i++){
+        if(visited[i]){
+            continue;
+        }
+
+        visited[i] = true;
+        answer.push_back(i);
+        select(num+1);
+        answer.pop_back();
+        visited[i] = false;
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+    select(1);
+    cout << min_cost;
+
+    // Please write your code here.
+
+    return 0;
+}

057cdd6148086bf4cb639931541bc490a8ccf738
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-01-16 18:49:26 +0900

    Title: 계단 오르기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260116/계단 오르기/climbing-stairs.cpp
+++ 260116/계단 오르기/climbing-stairs.cpp
@@ -6,12 +6,12 @@ int n;
 
 int main() {
     cin >> n;
-    dp[1] = 0;
+    dp[1] = 1;
     dp[2] = 1;
     dp[3] = 1;
     for(int i=4; i<=n; i++){
-        dp[i] += dp[i-3];
-        dp[i] += dp[i-2];
+        dp[i] = dp[i-3] + dp[i-2];
+        
     }
 
     cout << dp[n]%10007;

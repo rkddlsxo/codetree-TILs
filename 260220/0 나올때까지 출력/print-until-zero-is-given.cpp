5958bebbd4913f27aa3754386b5afc9f15df7710
rkddlsxo <68902901+rkddlsxo@users.noreply.github.com>
2026-02-20 23:31:19 +0900

    Title: 0 나올때까지 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260220/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int k;
+    while(1){
+        cin >> k;
+        if(k==0){
+            break;
+        }
+        cout << k;
+
+    }
+    return 0;
+}
